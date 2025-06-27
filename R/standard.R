5afc65d11a561c855d543aa4b13abac8fd67304f
MEWP (Matthew Phelps) <MEWP+nngithub@novonordisk.com>
2025-06-27 12:55:53 +0200

    Minor, change package argument to library

--- R/standard.R
+++ R/standard.R
@@ -1,8 +1,8 @@
 #' Retrieve mighty standard component
 #' @param standard standard
 #' @export
-get_standard <- function(standard, package) {
-  template <- find_standard(standard, package)
+get_standard <- function(standard, library) {
+  template <- find_standard(standard, library)
   mighty_standard$new(template = readLines(template))
 }
 
@@ -10,17 +10,17 @@ get_standard <- function(standard, package) {
 #' @param standard standard
 #' @param params list of input parameters
 #' @export
-get_rendered_standard <- function(standard, params, package) {
-  x <- get_standard(standard, package)
+get_rendered_standard <- function(standard, params, library) {
+  x <- get_standard(standard, library)
   do.call(what = x$render, args = params)
 }
 
 #' List all available standards
 #' @export
-list_standards <- function(package) {
+list_standards <- function(library) {
   templates <- system.file(
     "components",
-    package = package
+    package = library
   ) |> 
     list.files()
 
@@ -28,8 +28,8 @@ list_standards <- function(package) {
 }
 
 #' @noRd
-find_standard <- function(standard, package) {
-  if (!standard %in% list_standards(package)) {
+find_standard <- function(standard, library) {
+  if (!standard %in% list_standards(library)) {
     cli::cli_abort("Component {template} not found")
   }
 
