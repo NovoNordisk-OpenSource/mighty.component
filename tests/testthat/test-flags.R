9e6ccd40aa7f02cd345b471a283103e1f43073c9
akselthomsen <thomsenaksel@gmail.com>
2025-07-04 16:22:35 +0200

    fix: cleanup tests

--- tests/testthat/test-flags.R
+++
@@ -1,8 +0,0 @@
-test_that("trtemfl", {
-  pharmaverseadam::adae |>
-    dplyr::select(ASTDT, AENDT, TRTSDT, TRTEDT, OLD = TRTEMFL) |>
-    trtemfl(end_window = 30) |>
-    with(
-      expect_equal(object = TRTEMFL, expected = OLD, ignore_attr = TRUE)
-    )
-})
