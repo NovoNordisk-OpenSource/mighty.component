f676b16b625d14ecef49bf0c50ac44ef4d15efcf
akselthomsen <thomsenaksel@gmail.com>
2025-07-04 16:50:25 +0200

    fix: linting

--- tests/testthat/test-components.R
+++ tests/testthat/test-components.R
@@ -1,14 +1,17 @@
 test_that("predecessor", {
-  predecessor <- get_rendered_standard("predecessor", list(source = "pharmaverseadam::adsl", by = "USUBJID", variable = "ACTARM"))
+  predecessor <- get_rendered_standard(
+    "predecessor",
+    list(source = "pharmaverseadam::adsl", by = "USUBJID", variable = "ACTARM")
+  )
 
-  cat(predecessor$template, sep = "\n") |> 
+  cat(predecessor$template, sep = "\n") |>
     expect_snapshot()
 
   advs <- pharmaverseadam::advs |>
     dplyr::select(USUBJID, PARAMCD, AVAL, ACTARM)
 
   predecessor$test(
-    input = advs |> dplyr::select(-ACTARM), 
+    input = advs |> dplyr::select(-ACTARM),
     expected = advs
   )
 })
@@ -16,21 +19,21 @@ test_that("predecessor", {
 test_that("assign", {
   assign <- get_rendered_standard("assign", list(variable = "y", value = 1))
 
-  cat(assign$template, sep = "\n") |> 
+  cat(assign$template, sep = "\n") |>
     expect_snapshot()
 
   df <- data.frame(x = "a", y = 1)
 
   assign$test(
-    input = df |> dplyr::select(-y), 
+    input = df |> dplyr::select(-y),
     expected = df
   )
 })
 
 test_that("astdt", {
   astdt <- get_rendered_standard("astdt", list(dtc = "AESTDTC"))
-  
-  cat(astdt$template, sep = "\n") |> 
+
+  cat(astdt$template, sep = "\n") |>
     expect_snapshot()
 
   adae <- pharmaverseadam::adae |>
@@ -44,8 +47,8 @@ test_that("astdt", {
 
 test_that("aendt", {
   astdt <- get_rendered_standard("aendt", list(dtc = "AEENDTC"))
-  
-  cat(astdt$template, sep = "\n") |> 
+
+  cat(astdt$template, sep = "\n") |>
     expect_snapshot()
 
   adae <- pharmaverseadam::adae |>
@@ -60,7 +63,7 @@ test_that("aendt", {
 test_that("ady", {
   ady <- get_rendered_standard("ady", list(variable = "ASTDY", date = "ASTDT"))
 
-  cat(ady$template, sep = "\n") |> 
+  cat(ady$template, sep = "\n") |>
     expect_snapshot()
 
   adae <- pharmaverseadam::adae |>
@@ -74,17 +77,17 @@ test_that("ady", {
   ady$test(
     input = adae |> dplyr::select(-ASTDY),
     expected = adae |> dplyr::mutate(ASTDY = ASTDY + 1)
-  ) |> 
+  ) |>
     expect_error()
 })
 
 test_that("trtemfl", {
   trtemfl <- get_rendered_standard("trtemfl", list(end_window = 30))
 
-  cat(trtemfl$template, sep = "\n") |> 
+  cat(trtemfl$template, sep = "\n") |>
     expect_snapshot()
 
-  adae <- pharmaverseadam::adae |> 
+  adae <- pharmaverseadam::adae |>
     dplyr::select(ASTDT, AENDT, TRTSDT, TRTEDT, TRTEMFL)
 
   trtemfl$test(
