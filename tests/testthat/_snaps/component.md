cb48998a5c39cf2d8e0b6f678c8cfda7db7186a2
akselthomsen <thomsenaksel@gmail.com>
2025-07-15 15:31:16 +0200

    tests: upadte snapshots to use new print method

--- tests/testthat/_snaps/component.md
+++ tests/testthat/_snaps/component.md
@@ -1,24 +1,70 @@
 # get_rendered_component, custom code component as function multiple depends
 
-    {
-      "type": "character",
-      "attributes": {},
-      "value": ["", "    'hello' |> ", "      print()", "    if(a){", "    NULL", "    } else{", "      a <- 1", "      }", "      "]
-    }
+    Code
+      x
+    Message
+      <mighty_component_rendered/mighty_component/R6>
+      Type: derivation
+      Depends:
+      * .self.A
+      * lb.A
+      Outputs:
+      * B
+      Code:
+        'hello' |>
+          print()
+        if (a) {
+          NULL
+        } else {
+          a <- 1
+        }
+        
 
 # get_rendered_component custom local mustache template with params
 
-    {
-      "type": "character",
-      "attributes": {},
-      "value": ["", "    #' Analysis relative day", "", ".self <- .self |>", "  dplyr::mutate(", "    out_var = admiral::compute_duration(", "      start_date = TRTSDT,", "      end_date = date_var,", "      in_unit = '", "  days", "  ',", "      out_unit = '", "  days", "  ',", "      add_one = TRUE", "    )", "  )"]
-    }
+    Code
+      x
+    Message
+      <mighty_component_rendered/mighty_component/R6>
+      Type: derivation
+      Depends:
+      * .self.date_var
+      * .self.TRTSDT
+      Outputs:
+      * out_var
+      Code:
+      .self <- .self |>
+        dplyr::mutate(
+          out_var = admiral::compute_duration(
+            start_date = TRTSDT,
+            end_date = date_var,
+            in_unit = 'days',
+            out_unit = 'days',
+            add_one = TRUE
+          )
+        )
 
 # get_rendered_component returns rendered STANDARD code component with valid inputs
 
-    {
-      "type": "character",
-      "attributes": {},
-      "value": ["", ".self <- .self |>", "  dplyr::mutate(", "    out_var = admiral::compute_duration(", "      start_date = TRTSDT,", "      end_date = date_var,", "      in_unit = \"days\",", "      out_unit = \"days\",", "      add_one = TRUE", "    )", "  )"]
-    }
+    Code
+      x
+    Message
+      <mighty_component_rendered/mighty_component/R6>
+      Type: derivation
+      Depends:
+      * .self.date_var
+      * .self.TRTSDT
+      Outputs:
+      * out_var
+      Code:
+      .self <- .self |>
+        dplyr::mutate(
+          out_var = admiral::compute_duration(
+            start_date = TRTSDT,
+            end_date = date_var,
+            in_unit = "days",
+            out_unit = "days",
+            add_one = TRUE
+          )
+        )
 
--- tests/testthat/test-component.R
+++ tests/testthat/test-component.R
@@ -1,36 +1,12 @@
 test_that("get_rendered_component, custom code component as function multiple depends", {
-  # ARRANGE -------------------------------------------------------------------
-  tmp_file <- withr::local_tempdir() |>
-    file.path("ady.R")
-  # Custom code component as function
-  c(
-    "
-    #' Analysis relative day
-#' @description
-#' Derives the relative day compared to the treatment start date.
-#' @type derivation
-#' @depends .self A
-#' @depends lb A
-#' @outputs B
-    hello <- function(a){
-    'hello' |> 
-      print()
-    if(a){
-    NULL
-    } else{
-      a <- 1
-      }
-      return(.self)
-
-  }"
-  ) |>
-    writeLines(con = tmp_file)
   # ACT ---------------------------
-  x <- get_rendered_component(tmp_file)
+  x <- get_rendered_component(
+    component = test_path("_components", "ady.R")
+  )
 
   # ASSERT
   expect_s3_class(x, "mighty_component_rendered")
-  expect_snapshot_value(x$code, style = "json2")
+  expect_snapshot(x)
   expect_equal(x$type, "derivation")
   expect_equal(
     x$depends,
@@ -41,42 +17,9 @@ test_that("get_rendered_component, custom code component as function multiple de
 
 
 test_that("get_rendered_component custom local mustache template with params", {
-  # ARRANGE -------------------------------------------------------------------
-  tmp_file <- withr::local_tempdir() |>
-    file.path("ady_local.mustache")
-  # Create a mock mustache template
-  "
-    #' Analysis relative day
-#' Analysis relative day
-#' @description
-#' Derives the relative day compared to the treatment start date.
-#'
-#' @param variable `character` Name of new variable to create
-#' @param date `character` Name of date variable to use
-#' @type derivation
-#' @depends .self {{ date }}
-#' @depends .self TRTSDT
-#' @outputs {{ variable }}
-
-.self <- .self |>
-  dplyr::mutate(
-    {{ variable }} = admiral::compute_duration(
-      start_date = TRTSDT,
-      end_date = {{ date }},
-      in_unit = '
-  days
-  ',
-      out_unit = '
-  days
-  ',
-      add_one = TRUE
-    )
-  )
-" |>
-    writeLines(con = tmp_file)
   # ACT ---------------------------
   x <- get_rendered_component(
-    tmp_file,
+    component = test_path("_components", "ady_local.mustache"),
     params = list(
       date = "date_var",
       variable = "out_var"
@@ -85,7 +28,7 @@ test_that("get_rendered_component custom local mustache template with params", {
 
   # ASSERT
   expect_s3_class(x, "mighty_component_rendered")
-  expect_snapshot_value(x$code, style = "json2")
+  expect_snapshot(x)
   expect_equal(x$type, "derivation")
   expect_equal(
     x$depends,
@@ -99,7 +42,7 @@ test_that("get_rendered_component returns rendered STANDARD code component with 
   # ARRANGE -------------------------------------------------------------------
   # ACT ---------------------------
   x <- get_rendered_component(
-    "ady",
+    component = "ady",
     params = list(
       date = "date_var",
       variable = "out_var"
@@ -108,7 +51,7 @@ test_that("get_rendered_component returns rendered STANDARD code component with 
 
   # ASSERT ---------------------------
   expect_s3_class(x, "mighty_component_rendered")
-  expect_snapshot_value(x$code, style = "json2")
+  expect_snapshot(x)
   expect_equal(x$type, "derivation")
   expect_equal(
     x$depends,
