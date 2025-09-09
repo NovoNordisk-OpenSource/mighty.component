7f1d442488c4f256c746c852d08e664541b72555
MEWP (Matthew Phelps) <MEWP+nngithub@novonordisk.com>
2025-09-09 14:59:10 +0200

    minor

--- R/utils-validation.R
+++ R/utils-validation.R
@@ -1,13 +1,13 @@
 valid_types <- function() {
   # TODO: Update when we have a more fixed list
   # And hopefully more CDISC compliant
-  c("predecessor", "derivation", "row")
+  c("predecessor", "derivation", "row", "assigned")
 }
 
 assert_type <- function(type) {
   types <- valid_types()
   if (!type %in% types) {
-    cli::cli_abort("@type must be one of {.val {types}}")
+    cli::cli_abort("@type must be one of {.val {types}}. Found {.code type}")
   }
   return(type)
 }
