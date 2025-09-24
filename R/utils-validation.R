3b1df2d724cb8b544bf84590442b81a5e3006456
akselthomsen <thomsenaksel@gmail.com>
2025-09-24 12:37:22 +0200

    feat: split in type and origin metadata

--- R/utils-validation.R
+++ R/utils-validation.R
@@ -1,11 +1,29 @@
+#' @noRd
 valid_types <- function() {
-  c("predecessor", "derivation", "row")
+  c(
+    "column",
+    "row",
+    "bds"
+  )
 }
 
-assert_type <- function(type) {
-  types <- valid_types()
-  if (!type %in% types) {
-    cli::cli_abort("@type must be one of {.val {types}}")
+#' @noRd
+valid_origins <- function() {
+  c(
+    "Assigned",
+    "Collected",
+    "Derived",
+    "Not Available",
+    "Other",
+    "Predecessor",
+    "Protocol"
+  )
+}
+
+#' @noRd
+assert_choice <- function(x, choices, arg = deparse(substitute(x))) {
+  if (!x %in% choices) {
+    cli::cli_abort("@{arg} must be one of {.val {choices}}")
   }
-  return(type)
+  return(x)
 }
