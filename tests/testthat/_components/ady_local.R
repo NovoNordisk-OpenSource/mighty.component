6d5afeaffd9cf3a581f6017a9aa0a5c70e04063a
OATH (Aksel Thomsen) <OATH@novonordisk.com>
2025-10-31 10:53:49 +0100

    feat: R components from script (#58)
    * feat: R component as script instead of function
    
    * fix: bump version
    
    * fix: linting
    
    * fix: update snaps
    
    ---------
    
    Co-authored-by: akselthomsen <thomsenaksel@gmail.com>

---
+++ tests/testthat/_components/ady_local.R
@@ -0,0 +1,18 @@
+#' @title Analysis relative day
+#' @description
+#' Derives the relative day compared to the treatment start date.
+#' @type derivation
+#' @depends .self date_var
+#' @depends .self TRTSDT
+#' @outputs out_var
+#' @code
+.self <- .self |>
+  dplyr::mutate(
+    out_var = admiral::compute_duration(
+      start_date = TRTSDT,
+      end_date = date_var,
+      in_unit = "days",
+      out_unit = "days",
+      add_one = TRUE
+    )
+  )
---
+++ tests/testthat/_components/illegal_mustache.R
@@ -0,0 +1,6 @@
+#' @title Illegal use of mustache patterns
+#' @description
+#' Mustache patterns not allowed in custom R component
+#' @type derivation
+#' @code
+1 + {{ old_param }}
